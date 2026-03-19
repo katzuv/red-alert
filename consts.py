28aad3ba14aaf44286858d1deb0b7669eae73d51
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-03-19 18:18:47 +0200

    Add session string constant

--- consts.py
+++ consts.py
@@ -4,3 +4,4 @@ API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SOURCE_CHANNEL = "@beforeredalert"
 DESTINATION_CHANNEL = int(os.environ["DESTINATION_CHANNEL"])
+SESSION_STRING = os.environ["SESSION_STRING"]
