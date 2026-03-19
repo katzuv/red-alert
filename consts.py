0ce9d7c91da99a2f8abc921c72f0e376ff3e4fc3
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-03-19 23:08:08 +0200

    Save session string as session instead of string

--- consts.py
+++ consts.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from telethon.sessions import StringSession
 
 load_dotenv()
 
@@ -8,7 +9,7 @@ API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SOURCE_CHANNEL = "@beforeredalert"
 DESTINATION_CHANNEL = int(os.environ["DESTINATION_CHANNEL"])
-SESSION_STRING = os.environ["SESSION_STRING"]
+SESSION = StringSession(os.environ["SESSION_STRING"])
 
 MESSAGE_SUFFIX = """.
 🚨שתפו-https://t.me/beforeredalert"""
