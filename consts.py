bbb127ca93b31ad285393df3e568dfc68933b370
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-03-21 14:27:54 +0200

    Use shorter `getenv()` instead of `environ`

--- consts.py
+++ consts.py
@@ -5,13 +5,13 @@ from telethon.sessions import StringSession
 
 load_dotenv()
 
-API_ID = int(os.environ["API_ID"])
-API_HASH = os.environ["API_HASH"]
-SOURCE_CHANNEL = os.environ["SOURCE_CHANNEL"]
-DESTINATION_CHANNEL = int(os.environ["DESTINATION_CHANNEL"])
-SESSION = StringSession(os.environ["SESSION_STRING"])
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SOURCE_CHANNEL = os.getenv("SOURCE_CHANNEL")
+DESTINATION_CHANNEL = int(os.getenv("DESTINATION_CHANNEL"))
+SESSION = StringSession(os.getenv("SESSION_STRING"))
 
 MESSAGE_SUFFIX = """.
 🚨שתפו-https://t.me/beforeredalert"""
 
-MAX_QUEUE_SIZE = int(os.environ["MAX_QUEUE_SIZE"])
+MAX_QUEUE_SIZE = int(os.getenv("MAX_QUEUE_SIZE"))
