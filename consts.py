bbc339d5854d3e93091291c1ddf425e4d42be215
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-03-25 18:05:43 +0200

    Load environment variables as int or string if not a number

--- consts.py
+++ consts.py
@@ -5,13 +5,20 @@ from telethon.sessions import StringSession
 
 load_dotenv()
 
+def _load_as_int_or_str(env_var_name: str):
+    value = os.getenv(env_var_name)
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
 PHONE_NUMBER = os.getenv("PHONE_NUMBER")
 PASSWORD = os.getenv("TELEGRAM_PASSWORD")
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-SOURCE_CHANNEL = os.getenv("SOURCE_CHANNEL")
-DESTINATION_CHANNEL = int(os.getenv("DESTINATION_CHANNEL"))
+SOURCE_CHANNEL = _load_as_int_or_str("SOURCE_CHANNEL")
+DESTINATION_CHANNEL = _load_as_int_or_str("DESTINATION_CHANNEL")
 SESSION = StringSession(os.getenv("SESSION_STRING"))
 
 MESSAGE_SUFFIX = """.
